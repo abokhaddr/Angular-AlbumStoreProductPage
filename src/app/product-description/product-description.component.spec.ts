318b99f10f43a98da4e388d5a768a486430d884e
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-22 22:25:34 +0300

    Update product-description.component.spec.ts

--- src/app/product-description/product-description.component.spec.ts
+++ src/app/product-description/product-description.component.spec.ts
@@ -1,15 +1,3 @@
-<div class="row description">
-  <div class="col-sm-2">
-    <img src="assets/opacity-zero.jpg" alt="product image" />
-  </div>
-  <div class="col-sm-6">
-    <p class="album-name">Opacity Zero</p>
-    <p class="band-name">The Prependers</p>
-    <p class="album-release-date">September 25, 2014</p>
-  </div>
-</div>
- 25  src/app/product-description/product-description.component.spec.ts 
-@@ -0,0 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDescriptionComponent } from './product-description.component';
