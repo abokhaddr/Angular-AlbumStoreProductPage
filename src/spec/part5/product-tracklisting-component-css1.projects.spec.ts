7f63fda88e607a1328563875071b0299b6d665d2
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-22 22:47:33 +0300

    Update product-tracklisting-component-css1.projects.spec.ts

--- src/spec/part5/product-tracklisting-component-css1.projects.spec.ts
+++ src/spec/part5/product-tracklisting-component-css1.projects.spec.ts
@@ -1,46 +1,52 @@
-import { TestBed, async } from '@angular/core/testing';
+ const assert = require("chai").assert;
+const helpers = require("../helpers");
+const cssom = require("cssom");
+const _ = require("lodash");
 
-const CSSOM = require('cssom');
-const _ = require('lodash');
+describe("ProductTracklistingComponent", () => {
+  it("should have CSS that contains a .tracklisting selector @product-tracklisting-component-css1", () => {
+    helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.css",
+      "The ProductTracklistingComponent CSS file doesn't exist - have you run the `ng` command to generate it yet?"
+    );
 
-let productTracklistingCssFileExists = false;
-let productTracklistingCssFile;
-try {
-  productTracklistingCssFile = require('../../app/product-tracklisting/product-tracklisting.component.css');
-  productTracklistingCssFileExists = true;
-} catch (e) {
-  productTracklistingCssFileExists = false;
-}
+    const productTracklistingFile = helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.css"
+    );
 
-describe('ProductTracklisting', () => {
+    const styles = cssom.parse(productTracklistingFile);
 
-  it(`should have CSS that contains a .tracklisting selector @product-tracklisting-component-css1`, async(() => {
-    since('The ProductTracklistingComponent hasn\'t been created yet.').expect(productTracklistingCssFileExists).toBe(true);
-    if(productTracklistingCssFileExists) {
-      let parsed = CSSOM.parse(productTracklistingCssFile);
-      since('There isn\'t a `.tracklisting` selector in the ProductTracklistingComponent\'s CSS file right now.').expect(_.find(parsed.cssRules, {selectorText: '.tracklisting'})).not.toBeUndefined();
+    if (styles.cssRules.length == 0) {
+      assert(
+        false,
+        "The ProductTracklistingComponent file does not contain any CSS rules or there is a CSS syntax error."
+      );
     }
-  }));
+  });
 
-  it(`should have CSS with a rule setting the font-size to 16px and the padding-top to 10px on the .tracklisting selector @product-tracklisting-component-css1`, async(() => {
-    since('The ProductTracklistingComponent hasn\'t been created yet.').expect(productTracklistingCssFileExists).toBe(true);
-    if(productTracklistingCssFileExists) {
-      let parsed = CSSOM.parse(productTracklistingCssFile);
+  it(`should have CSS with a rule setting the font-size to 16px and the padding-top to 10px on the .tracklisting selector @product-tracklisting-component-css1`, () => {
+    const productTracklistingFile = helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.css"
+    );
+    const styles = cssom.parse(productTracklistingFile);
 
-      let tRule = _.find(parsed.cssRules, { selectorText: '.tracklisting' })
+    let tracklistingRule = _.find(styles.cssRules, {
+      selectorText: ".tracklisting"
+    });
 
-      since('There isn\'t a `.tracklisting` selector in the ProductTracklistingComponent\'s CSS file right now.').expect(tRule).not.toBeUndefined();
-      since('There isn\'t a `.tracklisting` selector in the ProductTracklistingComponent\'s CSS file right now.').expect(tRule.style.parentRule.selectorText).toBe('.tracklisting');
-      since('Your `.tracklisting` selector doesn\'t have a `font-size` property that\'s equal to `16px`.').expect(tRule.style['font-size']).toBe('16px');
-      if (tRule.style['padding-top']) {
-        since('Your `.tracklisting` selector isn\'t setting the top padding to be `10px`.').expect(tRule.style['padding-top']).toBe('10px');
-      } else if (tRule.style['padding']) {
-        let padding = tRule.style['padding'];
-        since('Your `.tracklisting` selector isn\'t setting the top padding to be `10px`.').expect(tRule.style['padding']).toBe('10px 0 0 0');
-      } else {
-        since('It doesn\'t look like you\'re setting the padding property in your `.tracklisting` selector.').expect(1).toBe(0);
-      }
-    }
-  }));
-  
+    assert(
+      tracklistingRule,
+      "There isn't a `.tracklisting` selector with its correct value in the ProductTracklistingComponent's CSS file right now."
+    );
+
+    assert(
+      tracklistingRule.style["font-size"] === "16px",
+      "Your `.tracklisting` tag selector doesn't have a `font-size` property that's equal to `16px`."
+    );
+
+    assert(
+      tracklistingRule.style["padding-top"] === "10px",
+      "Your `.tracklisting` tag selector doesn't have a `padding-top` property that's equal to `10px`."
+    );
+  });
 });
