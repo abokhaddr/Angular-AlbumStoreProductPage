4237b18ec93464eb0e4515c90248000770af524e
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-23 00:55:22 +0300

    Update product-tracklisting-html-moved.projects.spec.ts

--- src/spec/part5/product-tracklisting-html-moved.projects.spec.ts
+++ src/spec/part5/product-tracklisting-html-moved.projects.spec.ts
@@ -1,99 +1,77 @@
- const assert = require("chai").assert;
-const cheerio = require("cheerio");
-const parse5 = require("parse5");
-const helpers = require("../helpers");
+ import { TestBed, async } from '@angular/core/testing';
 
-describe("ProductPage", () => {
-  it("should have moved the tracklisting div out of the product-page component @product-tracklisting-html-moved", () => {
-    const productPageFile = helpers.readFile(
-      "src/app/product-page/product-page.component.html"
-    );
+import { AppModule } from '../../app/app.module';
 
-    // parse html to test for description div inside ProductPage Component
-    const productPageNodes = helpers.parseFile(productPageFile);
-    const productPageMain = helpers.getHtmlTag("main", productPageNodes);
-    const productPageContent = parse5.serialize(productPageMain[0]);
-    let $ = cheerio.load(productPageContent);
-    let tracklistingDiv = $(".tracklisting");
+import { Routes } from '@angular/router';
 
-    helpers.readFile(
-      "src/app/product-page/product-page.component.ts",
-      "The ProductPageComponent doesn't exist for some reason."
-    );
+import { RouterTestingModule } from '@angular/router/testing';
 
-    helpers.readFile(
-      "src/app/product-page/product-page.component.html",
-      "The ProductPageComponent HTML file doesn't exist for some reason."
-    );
+let productPageComponentExists = false;
+let ProductPageComponent;
+try {
+  ProductPageComponent = require('../../app/product-page/product-page.component.ts').ProductPageComponent;
+  productPageComponentExists = true;
+} catch (e) {
+  productPageComponentExists = false;
+}
 
-    helpers.readFile(
-      "src/app/product-tracklisting/product-tracklisting.component.html",
-      "The ProductTracklistingComponent doesn't exist - have you run the `ng` command to generate it yet?"
-    );
+let productTracklistingComponentExists = false;
+let ProductTracklistingComponent;
+try {
+  ProductTracklistingComponent = require('../../app/product-tracklisting/product-tracklisting.component.ts').ProductTracklistingComponent;
+  productTracklistingComponentExists = true;
+} catch (e) {
+  productTracklistingComponentExists = false;
+}
 
-    assert(
-      tracklistingDiv.length === 0,
-      "It looks like the ProductPageComponent still contains a `div` tag with a class of `tracklisting` - have you tried moving it yet?"
-    );
+describe('ProductPage', () => {
+  beforeEach(async(() => {
 
-    assert(
-      $(".row").length > 1,
-      "The ProductPageComponent should have two `<div></div>` elements with a class of `row`."
-    );
+    TestBed.configureTestingModule({
+      imports: [AppModule, RouterTestingModule.withRoutes([])],
+    }).compileComponents();
+  }));
 
-    assert(
-      $(".row")
-        .first()
-        .next()
-        .children(".col-sm-8").length > 0,
-      "The ProductPageComponent second `<div></div>` with a class of `row` should have a `<div></div>` with a class of `col-sm-8`."
-    );
+  it('should have moved the tracklisting div out of the product-page component @product-tracklisting-html-moved', async(() => {
+    since('The ProductTracklistingComponent doesn\'t exist - have you run the `ng` command to generate it yet?').expect(productTracklistingComponentExists).toBe(true);
+    since('The ProductPageComponent doesn\'t exist for some reason.').expect(productPageComponentExists).toBe(true);
 
-    assert(
-      $(".row")
-        .first()
-        .next()
-        .children(".col-sm-8")
-        .children("app-product-tracklisting").length > 0,
-      'You haven\'t added the `app-product-tracklisting` selector below the second `<div class="col-sm-8"></div>` element in the ProductPageComponent.'
-    );
-  });
+    const ProductPageFixture = TestBed.createComponent(ProductPageComponent);
+
+    if (ProductPageFixture.nativeElement.querySelector('app-product-tracklisting')) {
+      since('The ProductPageComponent has the `app-product-tracklisting` selector, but it still contains a `div` tag with a class of `tracklisting` - have you moved it over to the ProductTracklistingComponent yet?').expect(ProductPageFixture.nativeElement.querySelectorAll('div.tracklisting').length).toBe(1);
+    } else {
+      since('The ProductPageComponent still contains a `div` tag with a class of `tracklisting` - have you moved it over to the ProductTracklistingComponent yet?').expect(ProductPageFixture.nativeElement.querySelectorAll('div.tracklisting').length).toBe(0);
+    }
+  }));
+
+  it(`should contain the app-product-tracklisting element @product-tracklisting-html-moved`, async(() => {
+    since('The ProductPageComponent doesn\'t exist for some reason.').expect(productPageComponentExists).toBe(true);
+
+    const ProductPageFixture = TestBed.createComponent(ProductPageComponent);
+
+    since('You haven\'t added the `app-product-tracklisting` selector yet.').expect(ProductPageFixture.nativeElement.querySelector('app-product-tracklisting')).not.toBeNull()
+    since('You haven\'t added the `app-product-tracklisting` selector yet.').expect(ProductPageFixture.nativeElement.querySelector('app-product-tracklisting').nodeName).toBe('APP-PRODUCT-TRACKLISTING');    
+  }));
+  
+  
 });
 
-describe("ProductTracklisting", () => {
-  it("should contain the app-product-tracklisting element @product-tracklisting-html-moved", () => {
-    const productTracklistingFile = helpers.readFile(
-      "src/app/product-tracklisting/product-tracklisting.component.html"
-    );
+describe('ProductTracklisting', () => {
+  beforeEach(async(() => {
 
-    // parse html to test for tracklisting div inside Producttracklisting Component
-    let tracklisting;
-    const productTracklistingNodes = helpers.parseFile(productTracklistingFile);
-    productTracklistingNodes[0].attrs.find(
-      attr => (tracklisting = attr.value.match(/tracklisting/))
-    );
+    TestBed.configureTestingModule({
+      imports: [AppModule, RouterTestingModule.withRoutes([])],
+    }).compileComponents();
+  }));
 
-    let element;
-    try {
-      element = productTracklistingNodes[0].tagName;
-    } catch (e) {
-      assert(
-        "The ProductTracklistingComponent's HTML file doesn't contain a `div` tag with a class of `tracklisting`."
-      );
-    }
-    assert(
-      element !== "p",
-      "It looks like you have not replaced the `<p></p>` element with a `div` tag with a class of `tracklisting`."
-    );
+  it(`should have moved the tracklisting div into the product-tracklisting component @product-tracklisting-html-moved`, async(() => {
+    since('The ProductTracklistingComponent doesn\'t exist - have you run the `ng` command to generate it yet?').expect(productTracklistingComponentExists).toBe(true);
+    since('The ProductPageComponent doesn\'t exist for some reason.').expect(productPageComponentExists).toBe(true);
 
-    assert(
-      element === "div",
-      "The ProductTracklistingComponent's HTML file doesn't contain a `div` tag."
-    );
+    const ProductTracklistingFixture = TestBed.createComponent(ProductTracklistingComponent);
 
-    assert(
-      !!tracklisting,
-      "It looks like the ProductTracklistingComponent does not contain the `tracklisting` `<div></div>` from the ProductPageComponent."
-    );
-  });
+    since('The ProductTracklistingComponent\'s HTML file doesn\'t contain a `div` tag with a class of `tracklisting` - have you moved it over from the ProductPageComponent yet?').expect(ProductTracklistingFixture.nativeElement.querySelectorAll('div.tracklisting').length).toBeGreaterThan(0);
+  }));
+
 });
