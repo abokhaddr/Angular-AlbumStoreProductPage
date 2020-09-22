695f81ede7d7ec04f49dd280e57cb63cb2318e73
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-22 22:46:54 +0300

    Update product-tracklisting-component-created.projects.spec.ts

--- src/spec/part5/product-tracklisting-component-created.projects.spec.ts
+++ src/spec/part5/product-tracklisting-component-created.projects.spec.ts
@@ -1,18 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+ const helpers = require("../helpers");
 
-let productTracklistingComponentExists = false;
-let ProductTracklistingComponent;
-try {
-  ProductTracklistingComponent = require('../../app/product-tracklisting/product-tracklisting.component.ts').ProductTracklistingComponent;
-  productTracklistingComponentExists = true;
-} catch (e) {
-  productTracklistingComponentExists = false;
-}
-
-describe('Project', () => {
-
-  it(`should create the product tracklisting component @product-tracklisting-component-created`, async(() => {
-    since('The ProductTracklistingComponent doesn\'t exist - have you run the `ng` command to generate it yet?').expect(productTracklistingComponentExists).toBe(true);
-  }));
-
+describe("Project", () => {
+  it("should create the product tracklisting component @product-tracklisting-component-created", () => {
+    helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.ts",
+      "The ProductTracklistingComponent doesn't exist - have you run the `ng` command to generate it yet?"
+    );
+  });
 });
