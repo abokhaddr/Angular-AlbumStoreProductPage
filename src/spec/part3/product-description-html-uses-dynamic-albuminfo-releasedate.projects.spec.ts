0caf233cd973a242dab543fc331b46ee05e33d36
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-22 22:43:23 +0300

    Update product-description-html-uses-dynamic-albuminfo-releasedate.projects.spec.ts

--- src/spec/part3/product-description-html-uses-dynamic-albuminfo-releasedate.projects.spec.ts
+++ src/spec/part3/product-description-html-uses-dynamic-albuminfo-releasedate.projects.spec.ts
@@ -1,111 +1,35 @@
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+ const assert = require("chai").assert;
+const cheerio = require("cheerio");
+const parse5 = require("parse5");
+const helpers = require("../helpers");
 
-import { TestBed, async, inject } from '@angular/core/testing';
+describe("ProductDescription", () => {
+  it("should use release date data from the albumInfo property in the HTML template @product-description-html-uses-dynamic-albuminfo-releasedate", () => {
+    const productPageFile = helpers.readFile(
+      "src/app/product-description/product-description.component.html"
+    );
 
-import { AppModule } from '../../app/app.module';
+    const productPageNodes = helpers.parseFile(productPageFile);
+    const productPageMain = helpers.getHtmlTag("div", productPageNodes);
+    const productPageContent = parse5.serialize(productPageMain[0]);
+    let $ = cheerio.load(productPageContent);
+    let albumReleaseDate = $(".album-release-date");
 
-import { Http, BaseRequestOptions, Response, ResponseOptions, RequestOptions } from '@angular/http';
+    helpers.readFile(
+      "src/app/product-description/product-description.component.html",
+      "The ProductDescriptionComponent HTML file doesn't exist for some reason."
+    );
 
-import { MockBackend, MockConnection } from '@angular/http/testing';
+    assert(
+      albumReleaseDate.length > 0,
+      "Something happened and it looks like the ProductDescription HTML file does not contain a paragraph with a class of `album-release-date`."
+    );
 
-import { Observable } from 'rxjs/Observable';
-
-import { Routes } from '@angular/router';
-
-import { RouterTestingModule } from '@angular/router/testing';
-
-let json = require('../../assets/album.json');
-
-let productDescriptionComponentExists = false;
-let ProductDescriptionComponent;
-try {
-  ProductDescriptionComponent = require('../../app/product-description/product-description.component.ts').ProductDescriptionComponent;
-  productDescriptionComponentExists = true;
-} catch (e) {
-  productDescriptionComponentExists = false;
-}
-
-let productServiceExists = false;
-let ProductService;
-try {
-  ProductService = require('../../app/product.service.ts').ProductService;
-  productServiceExists = true;
-} catch (e) {
-  productServiceExists = false;
-}
-
-class AProductService {
-  
-}
-
-describe('ProductDescription', () => {
-
-  let product_service;
-  let ProvidedService;
-  let mock_backend;
-
-  if(productServiceExists) {
-    ProvidedService = ProductService
-  } else {
-    ProvidedService = AProductService;
-  }
-
-  beforeEach(async(() => {
-
-    TestBed.configureTestingModule({
-      imports: [AppModule, RouterTestingModule.withRoutes([])],
-      providers: [ProvidedService, MockBackend, BaseRequestOptions,
-        {
-          provide: Http,
-          useFactory: (mockBackend: MockBackend, defaultOptions: RequestOptions) => {
-            return new Http(mockBackend, defaultOptions);
-          },
-          useClass: Http,
-          deps: [MockBackend, BaseRequestOptions]
-        }
-      ]
-    }).compileComponents();
-  }));
-
-  beforeEach(inject([ProvidedService, MockBackend], (providedService, mockBackend) => {
-    product_service = providedService;
-    mock_backend = mockBackend;
-  }));
-  
-  it(`should use release date data from the albumInfo property in the HTML template @product-description-html-uses-dynamic-albuminfo-releasedate`, async(() => {
-    since('The ProductService hasn\'t been created yet.').expect(productServiceExists).toBe(true);
-    expect(productDescriptionComponentExists).toBe(true);
-
-    mock_backend.connections.subscribe((connection: MockConnection) => {
-      let options = new ResponseOptions({
-        body: json
-      });
-      connection.mockRespond(new Response(options));
-    });
-
-    const ProductDescriptionFixture = TestBed.createComponent(ProductDescriptionComponent);
-    ProductDescriptionFixture.detectChanges();
-
-    since('The release date in the ProductDescriptionComponent\'s HTML does not match the album name from the JSON response.').expect(ProductDescriptionFixture.debugElement.nativeElement.querySelector('.album-release-date').innerText).toEqual(json.album.releaseDate);
-
-    let htmlString = ""
-    try {
-      htmlString = require('../../app/product-description/product-description.component.html');
-    } catch (e) {
-    }
-    if (htmlString != "") {
-      const parser = new DOMParser();
-      const htmlDoc = parser.parseFromString(htmlString, 'text/xml');
-      const re = /{{\s*albumInfo\?\.album\.releaseDate\s*}}/
-      since('We\'d like you to query the albumInfo property directly for the release date, and we\'re not seeing that you\'re doing that.').expect(htmlDoc.querySelector('.album-release-date').textContent.match(re)).toEqual(jasmine.any(Array));
-    } else {
-      since('We\'d like you to query the albumInfo property directly for the release date, and we\'re not seeing that you\'re doing that.').expect(0).toBe(1);
-    }
-
-  }));
-
+    assert(
+      albumReleaseDate
+        .text()
+        .match(/{{\s*albumInfo\?\.album\.releaseDate\s*}}/),
+      "We'd like you to query the albumInfo property directly for the release date, and we're not seeing that you're doing that."
+    );
+  });
 });
