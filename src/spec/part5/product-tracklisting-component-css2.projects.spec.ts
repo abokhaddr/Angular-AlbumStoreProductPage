4762df66708c125cb215c6d206e29d7dabdf48bf
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-22 22:48:03 +0300

    Update product-tracklisting-component-css2.projects.spec.ts

--- src/spec/part5/product-tracklisting-component-css2.projects.spec.ts
+++ src/spec/part5/product-tracklisting-component-css2.projects.spec.ts
@@ -1,38 +1,56 @@
-import { TestBed, async } from '@angular/core/testing';
+ const assert = require("chai").assert;
+const helpers = require("../helpers");
+const cssom = require("cssom");
+const _ = require("lodash");
 
-const CSSOM = require('cssom');
-const _ = require('lodash');
+describe("ProductTracklistingComponent", () => {
+  it("should have CSS that contains a ul selector @product-tracklisting-component-css2", () => {
+    helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.css",
+      "The ProductTracklistingComponent CSS file doesn't exist - have you run the `ng` command to generate it yet?"
+    );
 
-let productTracklistingCssFileExists = false;
-let productTracklistingCssFile;
-try {
-  productTracklistingCssFile = require('../../app/product-tracklisting/product-tracklisting.component.css');
-  productTracklistingCssFileExists = true;
-} catch (e) {
-  productTracklistingCssFileExists = false;
-}
+    const productTracklistingFile = helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.css"
+    );
 
-describe('ProductTracklisting', () => {
+    const styles = cssom.parse(productTracklistingFile);
 
-  it(`should have CSS that contains a ul selector @product-tracklisting-component-css2`, async(() => {
-    since('The ProductTracklistingComponent hasn\'t been created yet.').expect(productTracklistingCssFileExists).toBe(true);
-    if(productTracklistingCssFileExists) {
-      let parsed = CSSOM.parse(productTracklistingCssFile);
-      since('There isn\'t a `ul` selector in the ProductTracklistingComponent\'s CSS file right now.').expect(_.find(parsed.cssRules, {selectorText: 'ul'})).not.toBeUndefined();
+    if (styles.cssRules.length == 0) {
+      assert(
+        false,
+        "The ProductTracklistingComponent file does not contain any CSS rules or there is a CSS syntax error."
+      );
     }
-  }));
 
-  it(`should have CSS with a rule setting the list-style-type to none on the ul selector @product-tracklisting-component-css2`, async(() => {
-    since('The ProductTracklistingComponent hasn\'t been created yet.').expect(productTracklistingCssFileExists).toBe(true);
-    if(productTracklistingCssFileExists) {
-      let parsed = CSSOM.parse(productTracklistingCssFile);
+    let ulRule = _.find(styles.cssRules, {
+      selectorText: "ul"
+    });
 
-      let ulRule = _.find(parsed.cssRules, { selectorText: 'ul' })
+    assert(
+      ulRule,
+      "There isn't an `ul` selector with its correct value in the ProductTracklistingComponent's CSS file right now."
+    );
+  });
 
-      since('There isn\'t a `ul` selector in the ProductTracklistingComponent\'s CSS file right now.').expect(ulRule).not.toBeUndefined();
-      since('There isn\'t a `ul` selector in the ProductTracklistingComponent\'s CSS file right now.').expect(ulRule.style.parentRule.selectorText).toBe('ul');
-      since('Your `ul` selector doesn\'t have a `list-style-type` property that\'s equal to `none`.').expect(ulRule.style['list-style-type']).toBe('none');
-    }
-  }));
-  
+  it(`should have CSS with a rule setting the list-style-type to none on the ul selector @product-tracklisting-component-css2`, () => {
+    const productTracklistingFile = helpers.readFile(
+      "src/app/product-tracklisting/product-tracklisting.component.css"
+    );
+    const styles = cssom.parse(productTracklistingFile);
+
+    let ulRule = _.find(styles.cssRules, {
+      selectorText: "ul"
+    });
+
+    assert(
+      ulRule,
+      "There isn't an `ul` selector with its correct value in the ProductTracklistingComponent's CSS file right now."
+    );
+
+    assert(
+      ulRule.style["list-style-type"] === "none",
+      "Your `ul` tag selector doesn't have a `list-style-type` property that's equal to `none`."
+    );
+  });
 });
