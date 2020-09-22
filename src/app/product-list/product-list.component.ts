eebbdfd07f63a3b28bf1ee4d760f0ca94afaf639
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-23 02:13:23 +0300

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,20 @@
- <ul>
-  <li *ngFor='let product of products'>
-    <a routerLink='/product/{{product.id}}' routerLinkActive='active'>{{product.albumName}}</a>
-  </li>
-</ul> 
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getProducts().subscribe(response => this.products = response);
+  }
+
+}
