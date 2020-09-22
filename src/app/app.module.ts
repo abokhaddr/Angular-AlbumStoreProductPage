09f964c3f39f1b4602e2d90546fb2b978216946b
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-09-22 23:29:24 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
- import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 
+import { ProductService } from './product.service'
+
 @NgModule({
   declarations: [
     AppComponent,
-
-    
     ProductPageComponent,
     ProductDescriptionComponent
   ],
@@ -20,7 +19,8 @@ import { ProductDescriptionComponent } from './product-description/product-descr
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
